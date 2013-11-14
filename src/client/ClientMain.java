baf6b9de4065892c567da081e70f01d71ccb218d
M0P <kommt-nicht-an@web.de>
2013-11-14 21:33:15 +0100

    added ping test

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
@@ -24,6 +25,7 @@ public class ClientMain {
         System.out.println(ip);
         AsynchronousSocketChannel client = null;
         try {
+            pingTest(ip);
             client = AsynchronousSocketChannel.open();
             client.connect(new InetSocketAddress(ip, 7777)).get();
             System.out.println(client.isOpen());
@@ -35,6 +37,21 @@ public class ClientMain {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch(ExecutionException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch(Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+    }
+
+    public void pingTest(String ip) throws Exception{
+        String ipAddress = "127.0.0.1";
+        InetAddress inet = InetAddress.getByName(ipAddress);
+
+        System.out.println("Sending Ping Request to " + ipAddress);
+        System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
+
+        inet = InetAddress.getByName(ip);
+
+        System.out.println("Sending Ping Request to " + ip);
+        System.out.println(inet.isReachable(5000) ? "Host is reachable" : "Host is NOT reachable");
     }
 }
