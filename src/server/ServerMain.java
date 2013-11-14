a0da5712aeb7fcb4f22f294e96d071d095daa85d
AarRei <starflyer11@web.de>
2013-11-14 20:25:49 +0100

    client server package

---
+++ src/server/ServerMain.java
@@ -0,0 +1,11 @@
+package server;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xenowar
+ * Date: 14.11.13
+ * Time: 20:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerMain {
+}
