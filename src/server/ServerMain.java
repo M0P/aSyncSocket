55a9a4b27a49bb5cd4f30063486bb4cd011d32f5
M0P <kommt-nicht-an@web.de>
2013-11-14 20:41:33 +0100

    first server code

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,11 +1,33 @@
 package server;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Xenowar
- * Date: 14.11.13
- * Time: 20:24
- * To change this template use File | Settings | File Templates.
- */
+import java.io.IOException;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 public class ServerMain {
+
+    public ServerMain() {
+        openServer();
+    }
+
+    public static void main(String[] args) {
+        new ServerMain();
+    }
+
+    public void openServer() {
+        try {
+            AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open().bind(null);
+            Future<AsynchronousSocketChannel> acceptFuture = server.accept();
+            AsynchronousSocketChannel worker = acceptFuture.get();
+            System.out.println(worker.getRemoteAddress());
+        } catch(IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch(InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch(ExecutionException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
 }
