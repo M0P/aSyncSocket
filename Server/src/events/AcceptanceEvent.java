f83cd16842615c04058df7c9e6eed4ed15057a24
AarRei <starflyer11@web.de>
2013-11-16 22:49:08 +0100

    eventkack

---
+++ Server/src/events/AcceptanceEvent.java
@@ -0,0 +1,11 @@
+package events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xenowar
+ * Date: 16.11.13
+ * Time: 22:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class AcceptanceEvent {
+}
---
+++ Server/src/events/AcceptanceEventListener.java
@@ -0,0 +1,11 @@
+package events;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Xenowar
+ * Date: 16.11.13
+ * Time: 22:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class AcceptanceEventListener {
+}
